f4c4d8d212b48fa63f34379b170101261d7a7381
Marco GP <anthonydeoz2@gmail.com>
2022-04-22 14:18:09 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import './App.css';
+import tasks from "./sample/tasks.json"
+// Components
+import Tasks from "./components/Tasks"
+import TaskForm from "./components/TaskForm";
+import Post from "./components/Post"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component{
+
+  state = {
+    tasks: tasks
+  }
+
+  addTask = (title, description) =>{
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    }) 
+  }
+
+  deleteTask = (id)=>{
+   const newTasks = this.state.tasks.filter(task => task.id !== id)
+   this.setState({tasks: newTasks})
+   console.log(newTasks);
+  }
+
+  checkDone = (id) =>{
+
+    const newTasks = this.state.tasks.map(task =>{
+      if (task.id === id){
+        task.done = !task.done
+      }
+      return task;
+    })
+    this.setState({tasks: newTasks})
+  }
+
+
+
+  render(){
+    return <div>
+      <Router>
+
+        <Link to="/">Home</Link>
+          <br/>
+          <Link to="/post">Posts</Link>
+
+        <Route exact path="/" render={() =>{
+        return  <div>
+            <TaskForm 
+            addTask={this.addTask}/>
+            <Tasks 
+            tasks={this.state.tasks} 
+            deleteTask={this.deleteTask}
+            checkDone={this.checkDone}/>
+              </div>
+        }}>
+        </Route>
+        <Route path="/post" component={Post}/>
+      </Router>
     </div>
-  );
+  }
+
 }
+
 
 export default App;
